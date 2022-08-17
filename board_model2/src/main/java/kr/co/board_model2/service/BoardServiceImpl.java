32ef68cf4665b0c363636f535485659926ca1ad7
ryuzakka <ryuzakkaya@gmail.com>
2022-08-17 17:16:23 +0900

    0817 수업 (mybatis-spring:scan) - SqlSession 선언 없이 DAO 바로 접근 가능

--- board_model2/src/main/java/kr/co/board_model2/service/BoardServiceImpl.java
+++ board_model2/src/main/java/kr/co/board_model2/service/BoardServiceImpl.java
@@ -16,17 +16,17 @@ import kr.co.board_model2.dto.BoardDto;
 public class BoardServiceImpl implements BoardService {
 
 	@Autowired
-	private SqlSession sqlSession;
+//	private SqlSession sqlSession;
+	private BoardDao dao;	// servlet-context.xml에 <mybatis-spring:scan> 태그의 기능으로 SqlSession을 사용하지 않고 곧바로 DAO를 선언하여 사용 가능하다.
 	
 	@Override
 	public String list(Model model) {
 		// model과 return값을 처리
-		BoardDao dao = sqlSession.getMapper(BoardDao.class);
+//		BoardDao dao = sqlSession.getMapper(BoardDao.class);
 		ArrayList<BoardDto> list = dao.list();
 		model.addAttribute("list", list);
 		
 		return "/list";
 	}
-	
 	
 }
